c2cf804a6ed3683fc61b664b1afae5e0f37576a5
nathaniel <nathaniel.simard.42@gmail.com>
2025-04-14 15:40:29 -0400

    Set rev

--- crates/burn-hip/src/lib.rs
+++ crates/burn-hip/src/lib.rs
@@ -18,16 +18,14 @@ pub type Hip<F = f32, I = i32, B = u8> = CubeBackend<HipRuntime, F, I, B>;
 #[cfg(feature = "fusion")]
 pub type Hip<F = f32, I = i32, B = u8> = burn_fusion::Fusion<CubeBackend<HipRuntime, F, I, B>>;
 
-// TODO: Hang the computer when AMD isn't available.
-//
-// #[cfg(target_os = "linux")]
-// #[cfg(test)]
-// mod tests {
-//     use burn_cubecl::CubeBackend;
-//
-//     pub type TestRuntime = cubecl::hip::HipRuntime;
-//     pub use half::f16;
-//
-//     // TODO: Add tests for bf16
-//     burn_cubecl::testgen_all!([f16, f32], [i8, i16, i32, i64], [u8, u32]);
-// }
+#[cfg(target_os = "linux")]
+#[cfg(test)]
+mod tests {
+    use burn_cubecl::CubeBackend;
+
+    pub type TestRuntime = cubecl::hip::HipRuntime;
+    pub use half::f16;
+
+    // TODO: Add tests for bf16
+    burn_cubecl::testgen_all!([f16, f32], [i8, i16, i32, i64], [u8, u32]);
+}
